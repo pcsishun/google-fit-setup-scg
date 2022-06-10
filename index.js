eee1731eb295ad7ddf68013034aa4621a9b71348
pcsishun <pcsishun@gmail.com>
2022-06-10 19:33:34 +0700

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const axios = require("axios");
 const {google} = require("googleapis");
 const urlParse = require("url-parse");
 const queryParse = require('query-string');
-
+const request = require('request');
+// const { response } = require("express");
 
 const app = express();
 app.use(express.urlencoded({extended:true}));
@@ -13,67 +14,118 @@ app.use(cors());
 
 const port = 8085;
 
-app.get("/testingfit", async (req, res) => {
+app.get("/testingfit", (req, res) => {
+
     const oauth2Client = new google.auth.OAuth2(
         "817348506088-l68q11t2r373vohlq2sr1jsj2n210ilf.apps.googleusercontent.com",
         "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",
         "http://localhost:8085/stepnext"
+        // "https://test-google-fit-dot-testdeploy-330007.as.r.appspot.com/stepnext"
     );
 
-    const scopes = ["https://www.googleapis.com/auth/fitness.activity.read", "https://www.googleapis.com/auth/fitness.location.read", "https://www.googleapis.com/auth/fitness.body.read"];
+    const scopes = ["https://www.googleapis.com/auth/fitness.activity.read profile email openid"];
+
+    // const scopes = ["https://www.googleapis.com/auth/fitness.activity.read",
+    // "https://www.googleapis.com/auth/fitness.location.read",
+    // "https://www.googleapis.com/auth/fitness.body.read",
+    // "https://www.googleapis.com/auth/fitness.nutrition.read",
+    // "https://www.googleapis.com/auth/fitness.blood_pressure.read",
+    // "https://www.googleapis.com/auth/fitness.body_temperature.read"];
+
+    // const scopes = ["https://www.googleapis.com/auth/fitness.activity.write"];
+
     const url = oauth2Client.generateAuthUrl({
-        access_type: "offline",
+        // access_type: "offline",
         scope:scopes,
         state:JSON.stringify({
             callbackUrl: req.body.callbackUrl,
             userID:req.body.userid
         })
     });
-    res.send(url)
+
+    
+    request(url, (err, response, body) => {
+        console.log('error: ' ,err);
+        console.log('statusCode: ', response && response.statusCode);
+        res.send({url});
+    })
+
+
 });
 
-app.get("/stepnext", async (req, res) => {
+app.get("/stepnext", async  (req, res) => {
+
     const queryUrl = new urlParse(req.url);
+    // console.log("queryUrl ===> ", queryUrl);
+
     const code = queryParse.parse(queryUrl.query).code;
+    // console.log("code ==> ", code);
 
     const oauth2Client = new google.auth.OAuth2(
         "817348506088-l68q11t2r373vohlq2sr1jsj2n210ilf.apps.googleusercontent.com",
         "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",
         "http://localhost:8085/stepnext"
+        // "https://test-google-fit-dot-testdeploy-330007.as.r.appspot.com/stepnext"
     );
+
     // console.log("code ===> ",code);
     // res.send(code);
 
     const tokens = await oauth2Client.getToken(code);
     // console.log("user access token ==> ",tokens);
-
     try{
 
         const headerConfig = {
             headers:{
-
+                "Content-Type":"application/json",
                 authorization:"Bearer " + tokens.tokens.access_token
             }
         }
         // 
         const payload = {
+
             aggregateBy:[
                 {
                     dataTypeName:"com.google.step_count.delta",
                     dataSourceId:"derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+                    // dataSourceId:"derived:com.google.activity.segment:com.google.android.gms:merge_activity_segments"
                 }
             ],
-            // bucketByTime:{durationMillis: 86400000},
-            // startTimeMillis:1585785599000,
-            // endTimeMillis:1585958399000
+            bucketByTime:{
+                durationMillis: 86400000
+            },
+            startTimeMillis:1654621200000,
+            endTimeMillis:1654863013121,
         }
+
         const result = await axios.post(
             "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
             payload,
             headerConfig
             );
-        console.log("none err ===>",result.data);
-        res.send(result.data);
+
+        const isResult = result.data.bucket;
+        
+        console.log(`isResult ==> ${isResult}`)
+        // let arrayData = [];
+
+        // isResult.forEach(element => {
+        //     const pointData =  element.dataset; 
+        //     pointData.forEach(arrayP => {
+        //         // const isValue = arrayP.value;
+        //         console.log(arrayP.value);
+        //         // isValue.forEach(arrayV => {
+        //         //     console.log(arrayV);
+        //         // })
+        //         if(arrayP.value === undefined){
+        //             arrayData.push("not found");
+        //         }else{
+        //             arrayData.push(arrayP.value)
+        //         }
+        //     })
+        // });
+        res.send(isResult);
+
     }catch(err){
         console.log("err ===> ",err);
         res.send(err);
